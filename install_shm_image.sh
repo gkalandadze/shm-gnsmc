7be6c032296f93b3f7c848a22c9f30c0e495f169
Giorgi Kalandadze <gkalandadze12@outlook.com>
2020-07-27 00:05:47 +0400

    small fix and added important comment

--- install_shm_image.sh
+++ install_shm_image.sh
@@ -10,33 +10,34 @@ crontab_command="@reboot docker start $container_name"
 docker_command="alias SHM='docker exec -it $container_name bash -i -c \"SHM\"'"
 
 # gadmoviwerot dockerisatvis sachiro secprofile rata chromma imushaos
-wget https://raw.githubusercontent.com/gkalandadze/shm-gnsmc/master/chrome.json -O ~/chrome.json
-
+if [ ! -f ~/docker_chrome.json ]; then
+    wget https://raw.githubusercontent.com/gkalandadze/shm-gnsmc/master/chrome.json -O ~/docker_chrome.json
+fi
 
 # gadmoviwerot shm is image docker hub idan (hub.docker.com)
 docker pull gkalandadze/shm-gnsmc:latest
 
 if [ $? -eq 0 ]; then
-    echo "-------------------------------------------------------"
+    echo "-------------------------------"
     echo "warmatebit gadmoiwera shm image"
-    echo "-------------------------------------------------------"
+    echo "-------------------------------"
 else
-    echo "---------------------------------------------"
+    echo "--------------------------"
     echo "ver gadmowera shm is image"
-    echo "---------------------------------------------"
+    echo "--------------------------"
     exit
 fi
 
 
 # shevamowmot 250 tan tu gaqvt kavshiri
 if ping -q -c 1 -W 1 10.0.0.250 >/dev/null; then
-    echo "--------------------------------------------------"
+    echo "---------------------------------"
     echo "servertan kavshiri shesadzlebelia"
-    echo "--------------------------------------------------"
+    echo "---------------------------------"
 else
-    echo "--------------------------------------------------------------------------------------------------------"
+    echo "-------------------------------------------------------------------"
     echo "servertan kavshiri sheudzlebelia, sheamowme 10.0.0.250 tan kavshiri"
-    echo "--------------------------------------------------------------------------------------------------------"
+    echo "-------------------------------------------------------------------"
     exit
 fi
 
@@ -69,6 +70,7 @@ docker volume create \
 
 
 # tu dockeris containeri ar aris sheqmnili mashin shevqmnat da gavushvat backgroundshi
+# zog sistemaze am brdzanebis gashveba sheidzleba iyos sachiro rata gui gamochndes: xhost +local:$(id -un)
 [[ $(docker ps -af "name=$container_name" --format '{{.Names}}') == $container_name ]] ||
 docker run -it -d --name $container_name -v data-250:/DATA \
     -v storage_01-250:/STORAGE/STORAGE_01 \
@@ -78,8 +80,8 @@ docker run -it -d --name $container_name -v data-250:/DATA \
     --memory 512mb \
     -v /tmp/.X11-unix:/tmp/.X11-unix \
     -e DISPLAY \
-    -v $HOME/Downloads:$/home/sysop/Downloads \
-    --security-opt seccomp=$HOME/chrome.json \
+    -v $HOME/Downloads:/home/sysop/Downloads \
+    --security-opt seccomp=$HOME/docker_chrome.json \
     --device /dev/snd \
     --device /dev/dri \
     -v /dev/shm:/dev/shm \
